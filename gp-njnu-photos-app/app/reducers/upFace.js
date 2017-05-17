eb8e1988759fdf610426cd295cbc720ddb4611ff
Moyu <492899414@qq.com>
2017-05-17 22:29:55 +0800

    update

--- gp-njnu-photos-app/app/reducers/upFace.js
+++ gp-njnu-photos-app/app/reducers/upFace.js
@@ -1,7 +1,7 @@
 import {Map, List} from 'immutable'
 const db = require('../common/storage')
 
-console.log("db.get('search_text')", db.get('search_text'));
+
 export const initState={
     activeSrc: 'camera',
     searchText: db.get('search_text') || '',
@@ -17,7 +17,9 @@ export const initState={
     },
     isStart: false,
     signId: ''
-}
+};
+
+console.log("db.get('search_text')", initState.searchText);
 
 const updateState = (state, key, subKey, newVal) => {
     let newState = Map(state)
